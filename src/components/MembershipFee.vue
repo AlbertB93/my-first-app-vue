8cf7941534d0c6c49105140ed99a61b7815d256f
Albert B <bercik93@interia.eu>
2023-01-04 21:32:48 +0100

    first commit

---
+++ src/components/MembershipFee.vue
@@ -0,0 +1,128 @@
+<template>
+  
+<div class="box">
+    <div class="person--container">
+      <h3>Lista zawodników oraz składek</h3>
+      <input class=" inputField" type="text" placeholder="Imię" v-model="newName">
+      <input class=" inputFieldNum" type="number" placholder="0" v-model="newAmount">
+<!--       ID <input class=" inputFieldNum" type="number" placholder="id" v-model="newId"> -->
+      <button @click="addPerson">Dodaj</button>
+
+      <personCom
+          v-for="person in persons" 
+          v-bind:key="person.id" 
+          :person="person"
+          @personPaidFee = 'personPaid'/>
+
+                          <p>Suma: </p>
+    </div>
+
+        <div class="person--container">
+          <h3>Lista wydatków</h3>
+          <input class=" inputField" type="text" placeholder="Nazwa" v-model="newName">
+          <input class=" inputFieldNum" type="number" placholder="0" v-model="newAmount">
+          ID <input class=" inputFieldNum" type="number" placholder="id" v-model="newId">
+          <button @click="addPerson">Dodaj</button>
+      
+        </div>
+</div>
+</template>
+
+<script>
+import personCom from './PersonPlayer.vue'
+export default {
+    components:{
+        personCom
+    },
+  data() {
+    return {
+      newName: '',
+      newAmount: 0,
+      newId: '',
+      persons: [
+        { name: 'Albert', amount: 0, id: 1, paidAmount: false },
+        { name: 'Marcin', amount: 40, id: 2, paidAmount: false },
+        { name: 'Kuba', amount: 50, id: 3, paidAmount: true },
+      ]
+    }
+  },
+  methods: {
+    addPerson() {
+      let testID= this.persons.length + 1;
+      console.log(testID);
+      this.persons.push({
+        name: this.newName,
+        amount: this.newAmount,
+        id: testID,
+        paidAmount:false,
+      })
+      this.newName = ''
+    },
+    updateAmount(id, amount) {
+      const index = this.persons.findIndex(el => el.id === id)
+      console.log(index)
+      console.log(amount)
+      /*      this.persons[index].amount += amount  */
+    },
+
+    
+     personPaid(id) {
+      const index = this.persons.findIndex(el => el.id === id)
+      console.log(index)
+/*       this.persons[index].paid == 50 */
+      this.persons[index].paidAmount = true
+    },
+  }
+}
+</script>
+
+<style>
+
+.box{
+  display: flex;
+  justify-content: center;
+}
+.app--container {
+  border: dotted 2px black;
+  text-align: center;
+  width: 88vw;
+}
+
+h2 {
+  text-align: center;
+}
+
+h3 {
+  text-align: center;
+}
+
+.person--container {
+  border: solid 2px black;
+  margin: 4px;
+  padding: 6px;
+  width: 40vw;
+}
+
+.inputField {
+  width: 15vw;
+}
+
+.inputFieldNum {
+  width: 4vw;
+}
+
+.person {
+  border: solid 1px black;
+  margin: 6px;
+  padding: 4px;
+}
+
+.person p {
+  font-size: 1em;
+}
+
+.paid {
+  opacity: 0.5;
+  text-decoration: line-through;
+}
+</style>
---
+++ src/components/PersonPlayer.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="person" v-bind:class="{
+        paid: person.paidAmount}" >
+        <p>
+            {{ person.id }}. Imię: {{ person.name }}, kwota: {{ person.amount }}
+        </p>
+        <button v-if="!person.paidAmount" @click="personPaid(person.id)"> Zapłacił
+        </button>
+    </div>
+    <div>
+
+    </div>
+</template>
+
+
+<script>
+export default{
+    props: ['person'],
+    methods: {
+        personPaid(){
+            this.$emit('personPaidFee',this.person.id)
+        }
+    }
+
+}
+</script>
+
+<style>
+.person {
+    border: solid 1px black;
+    margin: 6px;
+    padding: 4px;
+}
+
+.person p {
+    font-size: 1em;
+}
+</style>
